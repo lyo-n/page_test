79e53868ef18035a325b9dd19892a724d66633c0
lyo-n <1y0mmmm@gmail.com>
2023-01-19 10:21:04 +0200

    2d

---
+++ page_test/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+
+
+
+const Header = () => {
+    return (
+            <div className="header">
+               testHeader
+            </div>
+    );
+};
+
+export default Header;
